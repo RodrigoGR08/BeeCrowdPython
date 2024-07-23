f9b681bbde1967099fc8887321d43e3a90926f55
Rodrigo Garcia Ramalho <rodrigogarcia.ramalho@gmail.com>
2024-07-23 16:46:33 -0400

    Update Bee1042

--- Beginner/Bee1042.py
+++ Beginner/Bee1042.py
@@ -1,6 +1,4 @@
 #Read three integers and sort them in ascending order. After, print these values in ascending order, a blank line and then the values in the sequence as they were readed.
-def breakLine():
-    print("\n")
 n1, n2, n3 = map(int, input().split())
 numbers = [n1, n2, n3]
 sortedNumbers = numbers[:]
