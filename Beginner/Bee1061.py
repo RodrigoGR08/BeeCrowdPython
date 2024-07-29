e46fe877ccf0a10565cd721f1f5af45c18eee0c1
Rodrigo Garcia Ramalho <rodrigogarcia.ramalho@gmail.com>
2024-07-29 18:43:14 -0400

    Finishing event time

--- Beginner/Bee1061.py
+++ Beginner/Bee1061.py
@@ -1,49 +1,32 @@
-inputs = []
+day, startDate = input("").split()
+startDate = int(startDate)
+horaInicial, minutoInicial, segundoInicial  = map(int, input().split(":"))
 
-for i in range(4):
-    linha = input()
-    inputs.append(linha)
+day, endingDate = input("").split()
+endingDate = int(endingDate)
+horaFinal, minutoFinal, segundoFinal = map(int, input().split(":"))
 
-diaInicial = int(inputs[0][-1])
-horarioInicial = inputs[1].split(" : ")
-print(horarioInicial)
-for i in horarioInicial:
-    horarioInicial[i] = int(horarioInicial[i])
-print(horarioInicial)
-horaInicial = int(horarioInicial[0])
-minutosInicial, segundoInicial = int(inputs[1].split(" : "))
-diaFinal = int(inputs[2][-1])
-horaFinal, minutosFinal, segundoFinal = int(inputs[-1].split(" : "))
+totalSeconds = segundoFinal - segundoInicial
+totalMinutes = minutoFinal - minutoInicial
+totalHours = horaFinal - horaInicial
+totalDays = endingDate - startDate
+
+if(totalSeconds<0):
+    totalSeconds+=60
+    totalMinutes-=1
 
 
-totalDias = diaFinal-diaInicial
-#Hours
-if horaFinal < int(horarioInicial[0]):
-    totalDias-=1
-    totalHoras = (24-horaFinal) + horaInicial
-elif horaFinal == horaInicial:
-    totalHoras = 0
-else:
-    totalHoras = horaFinal-horaInicial
-#Minutes
-if minutosFinal < minutosInicial:
-    totalHoras-=1
+if(totalMinutes<0):
+    totalMinutes+=60
+    totalHours-=1
 
-    totalMinutos = (60-minutosFinal) + minutosInicial
-elif minutosFinal == minutosInicial:
-    totalMinutos = 0
-else:
-    totalMinutos = minutosFinal-minutosInicial
-#Seconds
-if segundoFinal < segundoInicial:
-    totalMinutos-=1
-    totalSegundo = (60-segundoFinal) + segundoInicial
-elif segundoFinal == segundoInicial:
-    totalSegundo = 0
-else:
-    totalSegundo = segundoFinal-segundoInicial
 
-print(f'''{totalDias} dia(s)
-      {totalHoras} hora(s)
-      {totalMinutos} minuto(s)
-      {totalSegundo} segundo(s)''')
+if(totalHours<0):
+    totalHours+=24
+    totalDays-=1
+
+
+print(f'''{totalDays} dia(s)
+{totalHours} hora(s)
+{totalMinutes} minuto(s)
+{totalSeconds} segundo(s)''')
